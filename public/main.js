eb45c19a798e3424d98823d15976e1fe12335cc8
AkwGaniu <lmd4sure@gmail.com>
2020-08-15 18:20:00 +0100

    initail commit

---
+++ public/main.js
@@ -0,0 +1,78 @@
+let peer = require('simple-peer')
+const Peer = require('simple-peer')
+let socket = io()
+const video = document.querySelector('vidoe')
+let client = {}
+
+//GET VIDEO STREEM
+navigator.mediaDevices.getUserMedia({video: true, audio: true})
+  .then((stream) => {
+    socket.emit('newClient')
+    video.srcObject = stream
+    video.play()
+    
+    // INITIALIZE A PEER
+    function initPeer(type) {
+      let peer = new Peer({initiator:(type === 'init')? true : false, stream: stream, trickle: false})
+      peer.on('stream', function (stream) {
+        createVideo(stream)
+      })
+
+      peer.on('close', function() {
+        document.getElementById('peerVideo').remove()
+        peer.destroy()
+      })
+      return peer
+    }
+
+    function removeVideo () {
+      document.getElementById('peerVideo').remove()
+    }
+
+    // PEER OF TYPE INIT
+    function makePeer() {
+      client.gotAnswer = false
+      let peer = initPeer('init')
+      peer.on('signal', function(data) {
+        if (!client.gotAnswer) {
+          socket.emit('offer', data)
+        }
+      })
+      client.peer = peer
+    }
+
+    // PEER OF TYPE NotInit 
+    function frontAnswer (offer) {
+      let peer = initPeer('notInit')
+      peer.on('signal', (data) => {
+        socket.emit('Answer', data)
+      })
+      peer.signal(offer)
+    }
+
+    function signalAnswer (answer) {
+      client.gotAnswer = true
+      let peer = client.peer
+      peer.signal(answwer)
+    }
+
+    function createVideo (stream) {
+      let video = document.createElement('video')
+      video.id = 'peerVideo'
+      video.srcObject = stream
+      video.class = 'embed-responsive-item'
+      document.querySelector('#peerDiv').appendChild(video)
+      video.play()
+    }
+
+    function sessionActive () {
+      document.write('Session Active, Please come back later')
+    }
+
+    socket.on('backOffer ', frontAnswer)
+    socket.on('backAnswer', signalAnswer)
+    socket.on('sessionActive', sessionActive)
+    socket.on('createPeer', makePeer)
+    socket.on('removeVideo', removeVideo)    
+  })
+  .catch(err => console.log(err))
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require ('socket.io')(http)
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + "/public"))
+let clients = 0
+
+io.on('connection', function (socket) {
+  socket.on('newClient', function () {
+    if (clients < 2) {
+      if (clients === 1) {
+        this.emit('creatPeer')
+      }
+    } else {
+      this.emit('sessionActive')
+    }
+    clients++
+  })
+
+  socket.on('offer', sendOffer)
+  socket.on('answer', sendAnser)
+  socket.on('disconnect', Disconnect)
+})
+
+
+function Disconnect() {
+  if (clients > 0) {
+    clients--
+    this.broadcast.emit('removeVideo')
+  }
+}
+
+function sendOffer(offer) {
+  this.broadcast.emit('backOffer', offer)
+}
+
+function sendAnser(data) {
+  this.broadcast.emit('backAnswer', offer)
+}
+
+http.listen(port, () => console.log(`Server active on port ${port}`))
