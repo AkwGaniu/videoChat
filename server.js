830da76d9a1649b173a48edc169e935bf9e0b987
AkwGaniu <lmd4sure@gmail.com>
2020-08-17 12:32:33 +0100

    ggg

--- server.js
+++ server.js
@@ -9,9 +9,8 @@ let clients = 0
 
 io.on('connection', function (socket) {
   socket.on('newClient', function () {
-    this.emit('hi')
     if (clients < 2) {
-      if (clients === 1) {
+      if (clients == 1) {
         this.emit('createPeer')
       }
     } else {
